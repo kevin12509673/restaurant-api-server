5311cf165a2c478db51cb7303be9a04a2e70c649
Kevin Li <kayiu99623@gmail.com>
2022-06-08 12:29:15 +0800

    Refactor: remove deprecated code

--- src/main/java/com/example/restaurantPlaform/userInfo/UserInfoService.java
+++ src/main/java/com/example/restaurantPlaform/userInfo/UserInfoService.java
@@ -20,14 +20,8 @@ public class UserInfoService {
   }
 
   public void saveUser(UserInfo user) throws IllegalStateException {
-    // if (user.getName() == null) throw new IllegalStateException("Name cannot be empty");
-    // if (user.getName() == null) throw new IllegalStateException(UserInfoServiceError.Null_Name.getDescription());
-    // if (user.getAge() == null) throw new IllegalStateException("Age cannot be empty");
-    // if (user.getDob() == null) throw new IllegalStateException("Date of birth cannot be empty");
-    // if (user.getEmail() == null) throw new IllegalStateException("Email cannot be empty");
-    // if (user.getPassword() == null) throw new IllegalStateException("Password cannot be empty");
-
-    if (userInfoRepository.findUserInfoByEmail(user.getEmail()).isPresent()) throw new IllegalStateException("User with the same email has been registered");
+    Boolean sameEmailIsPresent = userInfoRepository.findUserInfoByEmail(user.getEmail()).isPresent();
+    if (sameEmailIsPresent) throw new IllegalStateException("User with the same email has been registered");
 
     userInfoRepository.save(user);
   }
