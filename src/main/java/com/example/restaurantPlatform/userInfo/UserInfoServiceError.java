c73656fa590f5d2ac8a6cdf537a5cb1ddfcc9212
kevin12509673 <kayiu99623@gmail.com>
2022-06-17 12:41:42 +0800

    Refactor: rename enum properties

--- src/main/java/com/example/restaurantPlatform/userInfo/UserInfoServiceError.java
+++ src/main/java/com/example/restaurantPlatform/userInfo/UserInfoServiceError.java
@@ -1,11 +1,11 @@
 package com.example.restaurantPlatform.userInfo;
 
 public enum UserInfoServiceError {
-  Null_Name(1001, "User name is null"),
-  Null_Age(1002, "User age is null"),
-  Null_Dob(1003, "User date of birth is null"),
-  Null_Email(1004, "User email is null"),
-  Null_Password(1005, "User password is null");
+  NULL_NAME(1001, "User name is null"),
+  NULL_AGE(1002, "User age is null"),
+  NULL_DOB(1003, "User date of birth is null"),
+  NULL_EMAIL(1004, "User email is null"),
+  NULL_PASSWORD(1005, "User password is null");
 
   private Integer code;
   private String description;
