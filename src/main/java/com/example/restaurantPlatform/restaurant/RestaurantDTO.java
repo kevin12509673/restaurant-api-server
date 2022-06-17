70e146cfc70825ece1d9ea1f1c504b35fe493a25
kevin12509673 <kayiu99623@gmail.com>
2022-06-17 16:28:58 +0800

    Alter: add swagger annotation

--- src/main/java/com/example/restaurantPlatform/restaurant/RestaurantDTO.java
+++ src/main/java/com/example/restaurantPlatform/restaurant/RestaurantDTO.java
@@ -1,11 +1,22 @@
 package com.example.restaurantPlatform.restaurant;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.AccessMode;
 import lombok.Data;
 
+@ApiModel
 @Data
 public class RestaurantDTO {
+  @Schema(accessMode = AccessMode.READ_ONLY)
   private Long id;
+
+  @Schema(required = true)
   private String name;
+
+  @Schema(required = true)
   private String address;
+
+  @Schema(required = true)
   private Float rate;
 }
