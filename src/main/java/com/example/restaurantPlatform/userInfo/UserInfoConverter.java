1082e5e4e57cd3a7ef0fbda50660cbc116e16537
kevin12509673 <kayiu99623@gmail.com>
2022-06-17 15:14:16 +0800

    Alter: add UserInfoConverter and impl

---
+++ src/main/java/com/example/restaurantPlatform/userInfo/UserInfoConverter.java
@@ -0,0 +1,7 @@
+package com.example.restaurantPlatform.userInfo;
+
+import com.example.restaurantPlatform.converter.EntityConverter;
+
+public interface UserInfoConverter extends EntityConverter<UserInfoDTO, UserInfo> {
+
+}
